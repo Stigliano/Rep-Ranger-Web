1d5fd2d287b6af703decaa08dfa67f8c99f21e53
Stigliano Donatello <don.stigliano@gmail.com>
2026-01-07 12:28:58 +0100

    fix(terraform): add placeholder secret versions to unblock cloud run

--- infrastructure/secrets.tf
+++ infrastructure/secrets.tf
@@ -58,3 +58,20 @@ resource "google_secret_manager_secret" "audit_encryption_key" {
 # Esempio comando:
 # echo -n "your-password" | gcloud secrets versions add rapranger-db-password-prod --data-file=-
 
+# Placeholder versions per sbloccare il deploy (DA SOSTITUIRE MANUALMENTE IN PRODUZIONE)
+resource "google_secret_manager_secret_version" "db_password_init" {
+  secret      = google_secret_manager_secret.db_password.id
+  secret_data = "changeme_db_password"
+}
+
+resource "google_secret_manager_secret_version" "jwt_secret_init" {
+  secret      = google_secret_manager_secret.jwt_secret.id
+  secret_data = "changeme_jwt_secret_min_32_chars_long_string"
+}
+
+resource "google_secret_manager_secret_version" "jwt_refresh_secret_init" {
+  secret      = google_secret_manager_secret.jwt_refresh_secret.id
+  secret_data = "changeme_jwt_refresh_secret_min_32_chars_long"
+}
+
+
